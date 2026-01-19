6279926bcb250bebcd8179471642fc6051a6cb79
wlkaqw <1937393403@qq.com>
2026-01-19 20:19:24 +0800

    1

--- start0.cpp
+++ start0.cpp
@@ -3,6 +3,3 @@ int main() {
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
-void solve(){
-    
-}
---
+++ start1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,result;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    result=3*a-2*b+1;
+    cout<<"Result is: "<<result<<endl;
+}
